2e16debfaeae070500eb698879e11de9badc4155
Daniele Bianco <Daniele Bianco@F550L.station>
2018-06-11 23:32:18 +0200

    Punto 1 completato

--- src/it/polito/tdp/ufo/UfoController.java
+++ src/it/polito/tdp/ufo/UfoController.java
@@ -5,10 +5,13 @@
 package it.polito.tdp.ufo;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import it.polito.tdp.ufo.model.AnnoCount;
 import it.polito.tdp.ufo.model.Model;
+import it.polito.tdp.ufo.model.State;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -28,30 +31,71 @@ public class UfoController {
     private ComboBox<AnnoCount> boxAnno; // Value injected by FXMLLoader
 
     @FXML // fx:id="boxStato"
-    private ComboBox<?> boxStato; // Value injected by FXMLLoader
+    private ComboBox<State> boxStato; // Value injected by FXMLLoader
 
     @FXML // fx:id="txtResult"
     private TextArea txtResult; // Value injected by FXMLLoader
 
     @FXML
     void handleAnalizza(ActionEvent event) {
-
-    }
-
-    @FXML
-    void handleAvvistamenti(ActionEvent event) {
+    	
+    	txtResult.clear();
+    	
     	AnnoCount anno = this.boxAnno.getValue();
     	if(anno == null) {
     		this.txtResult.setText("Errore: selezionare un anno!\n");
     		return;
     	}
+    	
+    	State stato = this.boxStato.getValue();
+    	if(stato == null) {
+    		this.txtResult.setText("Errore: selezionare uno Stato!");
+    		return;
+    	}
+    	
+    	txtResult.appendText("Stati che precedono " + stato + ":\n");
+    	
+    	List<State> predecessori = model.mostraPredecessori(stato);
+    	for(State s : predecessori) {
+    		txtResult.appendText(s.toString()+"; ");
+    	}
+    	
+    	txtResult.appendText("\n\nStati che succedono " + stato + ":\n");
+    	
+    	List<State> successori = model.mostraSuccessori(stato);
+    	for(State s : successori) {
+    		txtResult.appendText(s.toString()+"; ");
+    	}
+    	
+    	txtResult.appendText("\n\nStati raggiungibili da " + stato +
+    			": (" + model.getNumNeighbours(stato)+ ")\n");
+    	
+    	Set<State> raggiungibili = model.displayNeighbours(stato);
+    	for(State r : raggiungibili) {
+    		txtResult.appendText(r.toString()+"; ");
+    	}
+    }
+
+    @FXML
+    void handleAvvistamenti(ActionEvent event) {
+    	
+    	AnnoCount anno = this.boxAnno.getValue();
+    	
+    	if(anno == null) {
+    		this.txtResult.setText("Errore: selezionare un anno!\n");
+    		return;
+    	}
+    	
     	model.creaGrafo(anno.getAnno());
+    	
+		this.boxStato.getItems().clear();
+    	this.boxStato.getItems().addAll(model.getStati());
     	
     }
 
     @FXML
     void handleSequenza(ActionEvent event) {
-
+    	
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -59,7 +103,8 @@ public class UfoController {
         assert boxAnno != null : "fx:id=\"boxAnno\" was not injected: check your FXML file 'Ufo.fxml'.";
         assert boxStato != null : "fx:id=\"boxStato\" was not injected: check your FXML file 'Ufo.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Ufo.fxml'.";
-
+        
+        txtResult.setStyle("-fx-font-family: monospace");
     }
 
 	public Model getModel() {
@@ -69,6 +114,7 @@ public class UfoController {
 	public void setModel(Model model) {
 		this.model = model;
 		this.boxAnno.getItems().addAll(model.getAvvistamenti());
+		this.boxStato.getItems().clear();
 	}
         
 }
--- src/it/polito/tdp/ufo/model/Model.java
+++ src/it/polito/tdp/ufo/model/Model.java
@@ -1,7 +1,10 @@
 package it.polito.tdp.ufo.model;
 
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
@@ -15,11 +18,11 @@ public class Model {
 	private List<AnnoCount> anniAvvistamenti;
 	private Graph<State,DefaultEdge> graph;
 	private StateIdMap stateIdMap ;
-	
-	
-	
+	private List<State> stati;
+		
 	public Model() {
 		this.stateIdMap = new StateIdMap();
+		this.stati = new ArrayList<State>();
 	}
 
 	public List<AnnoCount> getAvvistamenti(){
@@ -28,10 +31,14 @@ public class Model {
 		return anniAvvistamenti;
 	}
 	
+	public List<State> getStati() {
+		return stati;
+	}
+	
 	public void creaGrafo(Year anno) {
 		this.graph = new SimpleDirectedGraph<>(DefaultEdge.class);
 		SightingsDAO dao = new SightingsDAO();
-		List<State> stati = dao.getStati(anno, stateIdMap);
+		stati = dao.getStati(anno, stateIdMap);
 		Graphs.addAllVertices(graph, stati);
 		for (StateResult sr : dao.getStateResults(anno, stateIdMap)) {
 			Graphs.addEdgeWithVertices(graph, sr.getStato1(), sr.getStato2());
@@ -41,4 +48,41 @@ public class Model {
 				graph.edgeSet().size(), graph.vertexSet().size());
 	}
 	
+	public List<State> mostraPredecessori(State stato) {
+		return Graphs.predecessorListOf(graph, stato);
+	}
+
+	public List<State> mostraSuccessori(State stato) {
+		return Graphs.successorListOf(graph, stato);
+	}	
+			
+	public Set<State> displayNeighbours(State stato) {
+
+		Set<State> vicini = new HashSet<State>();
+
+		// Ottengo la lista dei vicini di un vertice
+		vicini.addAll(Graphs.neighborListOf(graph, stato));
+
+		// Ritorno la lista dei vicini
+		return vicini;
+	}
+
+	public String getNumNeighbours(State stato) {
+		return String.format("%d", graph.degreeOf(stato));
+	}
+	
+//	public Set<State> displayNeighboursBreadthFirstIterator(State stato) {
+//
+//		// trova il vertice di partenza
+//		State start = stateIdMap.get(stato);
+//
+//		// visita il grafo
+//		Set<State> visitati = new HashSet<>();
+//		BreadthFirstIterator<State, DefaultEdge> dfv = new BreadthFirstIterator<>(this.graph, start);
+//		while (dfv.hasNext())
+//			visitati.add(dfv.next());
+//
+//		return visitati;
+//	}
+
 }
